8a5541ef0951d88d5d01c3f03e1fe823d2097e3e
giovanni-centamori <gigiopaulino10@gmail.com>
2025-01-17 13:08:56 -0300

    Delete Target directory

--- Target/target5.py
+++
@@ -1,7 +0,0 @@
-string = input("Informe uma string: ")
-
-string_invertida = ""
-for char in string:
-    string_invertida = char + string_invertida
-
-print(f"String invertida: {string_invertida}")
